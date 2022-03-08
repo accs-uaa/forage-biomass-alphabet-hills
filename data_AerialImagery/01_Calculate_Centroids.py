43f6d8e8bc1ffa9c7a6f24b4321cc56cf9a6500f
Amanda Droghini <adroghini@alaska.edu>
2022-03-08 12:23:04 -0900

    Update 01_Calculate_Centroids.py
    Changed script to loop through all images, rather than a single image. Added final step to merge outputs into a single shapefile.

--- data_AerialImagery/01_Calculate_Centroids.py
+++ data_AerialImagery/01_Calculate_Centroids.py
@@ -9,6 +9,7 @@
 import arcpy
 from arcpy.sa import *
 import os
+import glob
 
 # Set root directory
 drive = 'F:\\'
@@ -23,7 +24,7 @@ imagery_folder = os.path.join(project_folder, 'Data_Input/imagery/aerial')
 processed_folder = os.path.join(imagery_folder, 'processed/rgb')
 temp_folder = os.path.join(imagery_folder, 'workspace')
 
-# Define working geodatabase
+# Define workspace and geodatabase
 work_geodatabase = os.path.join(project_folder, 'AlphabetHillsBrowseBiomass.gdb')
 arcpy.env.workspace = temp_folder
 
@@ -31,35 +32,60 @@ arcpy.env.workspace = temp_folder
 input_projection = 3338
 initial_projection = arcpy.SpatialReference(input_projection)
 
-# Testing script with single image
-
 # Define inputs
-input_raster = os.path.join(processed_folder,"DSC_1942.tif")
-file_name = "DSC_1942"
+input_list = glob.glob(os.path.join(processed_folder, '*.tif'))
 
-# Define outputs
-output_con = os.path.join(temp_folder,''.join([file_name,'_con','.tif']))
-output_polygon = os.path.join(temp_folder,''.join([file_name,'_poly','.shp']))
-output_point = os.path.join(temp_folder,''.join([file_name,'_pt','.shp']))
-output_point_project = os.path.join(temp_folder,''.join([file_name,'_pt','_proj','.shp']))
+# Define global outputs
 out_coor_system = arcpy.SpatialReference(4269)
+all_centroids = os.path.join(work_geodatabase, "aerial_centroids")
 
-# Convert a single-value raster
-print("Creating single-value raster...")
-con_raster = Con(input_raster, "1", "", "VALUE >= 0")
-con_raster.save(output_con)
+for i in range(len(input_list)):
 
-# Convert raster to polygon
-print("Converting raster to polygon...")
-arcpy.RasterToPolygon_conversion(output_con, output_polygon, "NO_SIMPLIFY")
+    input_raster = input_list[i]
+    # Create file name by removing extension and select last part of string
+    file_name = (os.path.splitext(input_raster)[0]).split('\\', -1)[-1]
+    print('Processing ' + file_name + ', image ' + str(i+1) + ' of ' + str(len(input_list)))
 
-# Feature to point
-print("Converting polygon to point...")
-arcpy.management.FeatureToPoint(output_polygon, output_point, "INSIDE")
+    # Define outputs
+    output_con = os.path.join(temp_folder, ''.join([file_name, '_con', '.tif']))
+    output_polygon = os.path.join(temp_folder, ''.join([file_name, '_poly', '.shp']))
+    output_point = os.path.join(temp_folder, ''.join([file_name, '_pt', '.shp']))
+    output_point_project = os.path.join(temp_folder, ''.join([file_name, '_pt', '_proj', '.shp']))
 
-# Project to NAD83
-arcpy.management.Project(output_point, output_point_project, out_coor_system)
+    # Convert a single-value raster
+    print("Creating single-value raster...")
+    con_raster = Con(input_raster, "1", "", "VALUE >= 0")
+    con_raster.save(output_con)
 
-# Add coordinate fields
-print("Adding coordinates...")
-arcpy.AddXY_management(output_point_project)
+    # Convert raster to polygon
+    print("Converting raster to polygon...")
+    arcpy.RasterToPolygon_conversion(output_con, output_polygon, "NO_SIMPLIFY")
+
+    # Feature to point
+    print("Converting polygon to point...")
+    arcpy.FeatureToPoint_management(output_polygon, output_point, "INSIDE")
+
+    # Project to NAD83
+    print("Changing projection...")
+    arcpy.Project_management(output_point, output_point_project, out_coor_system)
+
+    # Add coordinate fields
+    print("Adding coordinates...")
+    arcpy.AddXY_management(output_point_project)
+
+    # Add file name as a field in the attribute table
+    arcpy.CalculateField_management(output_point_project, field="Image_Name", expression='"' + file_name + '"', field_type="TEXT")
+
+# List all centroid files
+input_point_list = glob.glob(os.path.join(temp_folder, '*_pt_proj.shp'))
+
+# Change workspace to write to gdb
+arcpy.env.workspace = work_geodatabase
+
+# Combine all projected centroids into a single shapefile
+print ("Merging all centroids...")
+arcpy.Merge_management(inputs=input_point_list, output=all_centroids)
+
+# Remove extraneous fields
+drop_fields = ["Id", "gridcode", "ORIG_FID"]
+arcpy.DeleteField_management(all_centroids, drop_fields)
