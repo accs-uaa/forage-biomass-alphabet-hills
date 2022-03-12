d3c222f21004562d3637e3dcee96e74f83379045
Amanda Droghini <adroghini@alaska.edu>
2022-03-12 12:53:28 -0900

    Read in & join shapefile
    Read in geo-referenced shapefile and join to the metadata text file so that we have the original and geo-referenced coordinates all in one data frame. Fancy!

--- data_AerialImagery/02_PrepareCovariates_Metadata.R
+++ data_AerialImagery/02_PrepareCovariates_Metadata.R
@@ -3,14 +3,15 @@
 # Prepare Covariates from Aerial Imagery Metadata
 # Author: Amanda Droghini, Alaska Center for Conservation Science
 # Usage: Code chunks must be executed sequentially in R Studio or R Studio Server installation.
-# Description: "Prepare Covariates from Aerial Imagery Metadata" extracts data in text files and puts them in a dataframe. We'll use the variables in this dataframe to establish a statistical relationship between the original (text file) and geo-referenced (shapefile) centroid coordinates.
+# Description: "Prepare Covariates from Aerial Imagery Metadata" extracts data in text files and a shapefile, and joins them into a dataframe. We'll use the variables in this dataframe to establish a statistical relationship between the original (text files) and geo-referenced (shapefile) centroid coordinates.
 # ---------------------------------------------------------------------------
 
 # Import required libraries
 library(tidyverse)
+library(sf)
 
 # Set root directory
-drive = 'F:'
+drive = 'D:'
 root_folder = 'ACCS_Work'
 
 # Define input folders
@@ -20,10 +21,21 @@ image_subfolders <- list.dirs(image_folder)
 metadata_folders <- str_subset(image_subfolders, "METADATA")
 unprocessed_folder <- paste(image_folder, 'unprocessed/Images_RGB', sep = "/")
 
+# Define work geodatabase
+work_geodatabase <- paste(drive,root_folder,"Projects/WildlifeEcology/Moose_AlphabetHills/Data/AlphabetHillsBrowseBiomass.gdb", sep = "/")
+
+# Define input shapefile
+layer_name <- 'AerialImagery_Centroids'
+
 # Define output dataset
 output_csv = paste(image_folder,
                    'AerialImagery_Metadata.csv',
                    sep = '/')
+
+# Read in shapefile
+centroids <- st_read(dsn=work_geodatabase,layer=layer_name)
+
+# Extract metadata from text files ----
 
 # List variables of interest in the metadata files
 variables <- c("imageNumber","latitude","longitude","height","track","speed")
@@ -65,6 +77,12 @@ all_metadata <- rowid_to_column(all_metadata)
 
 # Append image name
 all_metadata$fileName <- image_names
+
+# Join metadata and shapefile ----
+# Only georeferenced images will have a value for POINT_X and POINT_Y
+metadata_join <- left_join(x=all_metadata, y=centroids,by = c("fileName" = "Image_Name")) %>% 
+  select(-Shape) %>% 
+  rename(georef_latitude = POINT_Y, georef_longitude = POINT_X)
 
 # Export as CSV
 write_csv(all_metadata, file = output_csv)
